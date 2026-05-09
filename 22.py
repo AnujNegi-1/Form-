598cdf029000a3053fd3127fba7a16ec22c46494
AnujNegi-1 <154433731+AnujNegi-1@users.noreply.github.com>
2026-05-09 16:22:48 +0530

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,73 @@
+from tkinter import *
+from tkinter import ttk
+a=Tk()
+a.geometry("1000x1000")
+a.resizable(False,False)
+a.title("First Code")
+Label(a,text="User Form", font="Algerian 25",bg="dark red",fg="white").pack(fill="both")
+
+Label(a,text="Name", font="Algerian 16",fg="black").place(x=120,y=125)
+Label(a,text="Clas", font="algerian 16", fg="black").place(x=120, y=225)
+Label(a,text="Session", font="algerian 16", fg="black").place(x=320, y=225)
+Label(a,text="School Name", font="algerian 16", fg="black").place(x=570, y=225)
+Label(a,text="Contact No.", font="algerian 16", fg="black").place(x=120, y=275)
+
+
+nm=StringVar()
+nm1=StringVar()
+cl=StringVar()
+cl1=StringVar()
+se=StringVar()
+se1=StringVar()
+scl=StringVar()
+cn=StringVar()
+
+#Combobox
+
+#Name
+nm=StringVar()
+name = ttk.Combobox(a, width = 10, textvariable = nm)
+name['values'] = (' Mr'
+                  ' Ms'
+                  ' Mrs')
+
+name.place(x=220,y=125)
+name.current()
+
+nm1=Entry(a,bd="2",textvariable=nm1)
+nm1.place(x=400,y=125)
+
+#Class
+cl=StringVar()
+clas = ttk.Combobox(a, width = 10, textvariable = cl)
+clas['values'] = (' 10th'
+                   ' 12th')
+
+clas.place(x=210,y=225)
+clas.current()
+
+#Session
+se=StringVar()
+session = ttk.Combobox(a, width = 10, textvariable = se)
+session['values'] = (' 1998'
+                     ' 1999'
+                     ' 2000'
+                     ' 2001'
+                     ' 2002'
+                     ' 2003'
+                     ' 2004'
+                     ' 2005'
+                     ' 2006'
+                     ' 2007')
+                     
+session.place(x=460,y=225)
+session.current()
+
+#School Label
+scl1=Entry(a,bd='3',textvariable=cn)
+scl1.place(x=740,y=225)
+
+cn1=Entry(a,bd="2",textvariable=cn)
+cn1.place(x=300,y=275)
+
+mainloop()
